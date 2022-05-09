5aefa0048457dbe9f3e6434cd30ddc06e93add5f
ajitesh <ajitesh314mishra@gmail.com>
2022-05-09 18:39:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+// https://www.youtube.com/watch?v=oiX-6Y2oGjI
+const App = () => {
 
-function App() {
+  const [calc, setCalc] = useState("")
+  const [result, setResult] = useState("")
+
+
+
+  //  arrays of oprators
+
+  const ops = ['/', '*', '+', '-', '.']
+
+
+  //  function to handle the input
+
+  const updateCalc = (value) => {
+    if (
+      ops.includes(value) && calc === "" ||
+      ops.includes(value) && ops.includes(calc.slice(-1))
+    ) {
+      return
+    }
+    setCalc(calc + value)
+
+    if (!ops.includes(value)) {
+      setResult(eval(calc + value).toString())
+    }
+  }
+
+  const Calculate = () => {
+    setCalc(eval(calc).toString())
+  }
+
+  // function to delete the last input
+  const Del = () => {
+    if(calc === ""){
+      return
+    }
+
+    const value = calc.slice(0, -1)
+    setCalc(value)
+    setResult(eval(value).toString())
+  }
+
+  const Clear = () => {
+    setCalc("")
+    setResult("")
+
+  }
+
+  // function to create digits just like array.map inside jsx
+
+
+  const createDigit = () => {
+    const digit = []
+
+    for (let i = 1; i < 10; i++) {
+      digit.push(<button onClick={() => updateCalc(i.toString())} key={i}>{i}</button>)
+    }
+
+    return digit
+
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='calculator_parent'>
+      <div className="calculator">
+        <div className="display_result">
+          {result ? <span>({result})</span> : ""} {calc || '0'}
+        </div>
+
+        <div className="operators">
+          <button onClick={() => updateCalc("/")}>/</button>
+          <button onClick={() => updateCalc("*")}>X</button>
+          <button onClick={() => updateCalc("+")}>+</button>
+          <button onClick={() => updateCalc("-")}>-</button>
+          <button onClick={Del}>DEL</button>
+          <button onClick={Clear}>CLR</button>
+
+        </div>
+
+        <div className="digits">
+          {createDigit()}
+          <button onClick={() => updateCalc("0")}>0</button>
+          <button onClick={() => updateCalc(".")}>.</button>
+          <button onClick={Calculate}>=</button>
+
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
